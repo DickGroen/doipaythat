b3eb30a25e15c23dc724906149e9af52c6271601
DickGroen <dickgroen2@gmail.com>
2026-04-28 23:41:55 +0200

    Export multiple prompts from marketing index

--- prompts/marketing/index.js
+++ prompts/marketing/index.js
@@ -1,1 +1,5 @@
-
+export { pricingPrompt } from "./pricing.js";
+export { landingPrompt } from "./landing.js";
+export { checkoutPrompt } from "./checkout.js";
+export { upsellPrompt } from "./upsell.js";
+export { emailPrompt } from "./email.js";
