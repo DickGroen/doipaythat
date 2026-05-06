3b4574fb73002b8cf2126856b711686f3c0a3856
DickGroen <dickgroen2@gmail.com>
2026-05-06 11:11:54 +0200

    Add escapeHtml function to escape HTML characters

---
+++ worker/utils/html.js
@@ -0,0 +1,10 @@
+// worker/utils/html.js
+
+export function escapeHtml(str) {
+  return String(str || "")
+    .replaceAll("&",  "&amp;")
+    .replaceAll("<",  "&lt;")
+    .replaceAll(">",  "&gt;")
+    .replaceAll('"',  "&quot;")
+    .replaceAll("'",  "&#039;");
+}
